2ff7364b4ab6653dfcf1bf88b2dd97dd0870488e
Elena Fadeeva <ms.elenafadeeva@gmail.com>
2023-08-31 15:58:28 +0200

    Added preprocess_features function

--- eficient_frontier/ml_logic/preprocessor.py
+++ eficient_frontier/ml_logic/preprocessor.py
@@ -2,12 +2,20 @@ import numpy as np
 import pandas as pd
 import math
 
-from sklearn.pipeline import make_pipeline
+import seaborn as sns
+import matplotlib.pyplot as plt
+pd.set_option('display.max_columns', None)
+from statsmodels.graphics.gofplots import qqplot
+from sklearn.preprocessing import RobustScaler, StandardScaler, MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.impute import SimpleImputer, KNNImputer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.compose import ColumnTransformer, make_column_transformer
-from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
+from sklearn.compose import make_column_selector
 
 
-def preprocess_features(X: pd.DataFrame) -> np.ndarray:
+def preprocess_features(data_normal: pd.DataFrame) -> np.ndarray:
     def create_sklearn_preprocessor() -> ColumnTransformer:
         """
         Scikit-learn pipeline that transforms a cleaned dataset
@@ -15,14 +23,64 @@ def preprocess_features(X: pd.DataFrame) -> np.ndarray:
         Stateless operation: "fit_transform()" equals "transform()".
         """
 
+        # Numerical features
+        robust_features = ['INCOME', 'NETWORTH']
+        standard_features = ['AGE']
+        minmax_features = ['KIDS']
 
-        return "final_preprocessor"
+        scalers = ColumnTransformer([
+            ("robust_scaler", RobustScaler(), robust_features),
+            ("standard_scaler", StandardScaler(), standard_features),
+            ("minmax_scaler", MinMaxScaler(), minmax_features),
+        ], remainder="passthrough").set_output(transform='pandas')
+
+        num_transformer = Pipeline([
+            ("num_imputer", SimpleImputer(strategy="median")),
+            ("scalers", scalers)
+        ])
+
+        # Categorical features
+        ordinal_features = []
+        ohe_features = ['HHSEX', 'MARRIED', 'OCCAT1', 'WSAVED', 'FAMSTRUCT']
+
+        encoders = ColumnTransformer([
+            ("ordinalencoder", OrdinalEncoder(), ordinal_features),
+            ("onehotencoder", OneHotEncoder(drop='if_binary',
+                                            sparse_output=False,
+                                            handle_unknown="ignore",
+                                            min_frequency=5), ohe_features),
+        ], remainder="passthrough")
+
+        cat_transformer = Pipeline([
+            ("cat_imputer", SimpleImputer(strategy="most_frequent")),
+            ("encoders", encoders)
+        ])
+
+        # Column Transformer
+        num_features = ['AGE', 'KIDS', 'INCOME', 'NETWORTH']
+        cat_features = ['HHSEX', 'MARRIED', 'OCCAT1', 'WSAVED', 'FAMSTRUCT', 'YESFINRISK', 'EDCL']
+
+        final_preprocessor = ColumnTransformer([
+            ("num_transformer", num_transformer, num_features),
+            ("cat_transformer", cat_transformer, cat_features),
+        ], remainder="passthrough").set_output(transform="pandas")
+
+        return final_preprocessor
 
     print("\nPreprocessing features...")
 
+    # Train test split
+    y = data_normal['RT']
+    X = data_normal.drop(columns=['RT'])
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
     preprocessor = create_sklearn_preprocessor()
-    X_processed = preprocessor.fit_transform(X)
+    preprocessor.fit(X_train)
+    X_train_preprocessed = preprocessor.transform(X_train)
+    X_test_preprocessed = preprocessor.transform(X_test)
 
-    print("✅ X_processed, with shape", X_processed.shape)
+    print("✅ X_train_preprocessed, with shape", X_train_preprocessed.shape)
+    print("✅ X_test_preprocessed, with shape", X_test_preprocessed.shape)
 
-    return X_processed
+    return X_train_preprocessed, X_test_preprocessed, y_train, y_test
